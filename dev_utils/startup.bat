55803ea3a2c280e9bb265d5b39113eab8345de50
ymmmtym <h.yukihisa.378@gmail.com>
2020-01-30 07:43:24 +0900

    add window bat file

---
+++ dev_utils/startup.bat
@@ -0,0 +1,4 @@
+### start vagrant automatically for VirtualBox Windows Ver.
+
+start "" /b "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" -startvm "Ubuntu_v18"
+start "" /b "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" -startvm "${vm_name}"
