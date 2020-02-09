649ee5cfa0ea137c2ffc844c5eb2f006ce893d8e
ymmmtym <h.yukihisa.378@gmail.com>
2020-02-09 18:12:01 +0900

    update git util

--- dev_utils/git_clone_repos.sh
+++ dev_utils/git_clone_repos.sh
@@ -6,9 +6,10 @@ count=$(($(echo $json | jq '. | length') - 1))
 echo "Follow repos are found"
 echo $json | jq .[].name | nl
 echo -n "clone repository number: "
-read i
+read nums
+for i in ${nums}; do
 number=$((i - 1))
 git clone $(echo $json | jq -r .[$number].ssh_url)
 echo "Done!"
-
+done
 exit 0
