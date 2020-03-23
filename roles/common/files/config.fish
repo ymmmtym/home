d03001312848dada1bf2476364a8057fcfecf4b3
ymmmtym <h.yukihisa.378@gmail.com>
2020-03-24 07:40:12 +0900

    add fish config

---
+++ roles/common/files/config.fish
@@ -0,0 +1,15 @@
+# alias
+alias egrep 'egrep --color=auto'
+alias fgrep 'fgrep --color=auto'
+alias grep 'grep --color=auto'
+alias l ls
+alias ll 'ls -l'
+alias lla 'ls -al'
+alias ls 'ls -FG'
+
+# alias tmux
+alias tat 'tmux a -t'
+alias tkt 'tmux kill-session -t'
+alias tkt-all 'tmux kill-server'
+alias tl 'tmux ls'
+alias tnt 'tmux new -t'
