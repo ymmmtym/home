476618afdd69ae60d6ebb3078cd8803c7bf933e3
ymmmtym <h.yukihisa.378@gmail.com>
2020-03-26 00:26:44 +0900

    delete dev_utils

--- dev_utils/startup.bat
+++
@@ -1,4 +0,0 @@
-### start vagrant automatically for VirtualBox Windows Ver.
-
-start "" /b "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" -startvm "Ubuntu_v18"
-start "" /b "C:\Program Files\Oracle\VirtualBox\VirtualBox.exe" -startvm "${vm_name}"
