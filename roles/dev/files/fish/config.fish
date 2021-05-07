f5b86de09193270b0b418f52cda7a320383ab83c
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2021-05-07 21:56:28 +0900

    update fish tasks

--- roles/dev/files/fish/config.fish
+++ roles/dev/files/fish/config.fish
@@ -1,3 +1,10 @@
+# fish
+set -x fish_theme agnoster
+function fish_user_key_bindings
+  bind \cr 'peco_select_history (commandline -b)'                                                                                                                                
+  bind \c] peco_select_ghq_repository
+end
+
 # alias
 alias egrep 'egrep --color=auto'
 alias fgrep 'fgrep --color=auto'
