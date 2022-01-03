845fcc02e2652baaf6f3cc92e649a30e6be36c20
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2022-01-03 11:58:50 +0900

    update inventories

--- inventories/terraform_inventory_esxi_from_tfstate.py
+++ inventories/terraform_inventory_esxi_from_tfstate.py
@@ -13,11 +13,12 @@ RESOURCES = TFSTATE['resources']
 
 for resource in RESOURCES:
     if resource['type'] == 'esxi_guest':
-        host = resource['name']
-        INVENTORY['all']['hosts'].append(host)
-        INVENTORY['_meta']['hostvars'][host] = resource['instances'][0]
-        INVENTORY['_meta']['hostvars'][host]['ansible_host'] \
-            = resource['instances'][0]['attributes']['ip_address']
+        for instance in resource['instances']:
+            host = instance['attributes']['guest_name']
+            INVENTORY['all']['hosts'].append(host)
+            INVENTORY['_meta']['hostvars'][host] = instance
+            INVENTORY['_meta']['hostvars'][host]['ansible_host'] \
+                = instance['attributes']['ip_address']
 
 
 print(json.dumps(INVENTORY, indent=4))
