bb675ae27c59fb249cacaf95ded9b31f8851510d
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2022-04-26 22:32:49 +0900

    add haproxy

--- terraform/guest-rke2.tf
+++ terraform/guest-rke2.tf
@@ -8,8 +8,9 @@ locals {
   ssh_authorized_key = file("~/.ssh/kube.id_rsa.pub")
 
   nodes = {
-    server = 3
-    agent  = 3
+    haproxy = 2
+    server  = 3
+    agent   = 3
   }
 }
 
@@ -24,12 +25,50 @@ resource "esxi_resource_pool" "kubernetes" {
   cpu_shares         = "normal"
   mem_min            = local.memsize * 2
   mem_min_expandable = "false"
-  mem_max            = local.memsize * 7
+  mem_max            = local.memsize * 12
   mem_shares         = "normal"
 }
 
 data "template_file" "rke2_metadata" {
   template = file("cloud-init/meta-data-ubuntu.yml")
+}
+
+
+### HAProxy for RKE2 Server
+
+data "template_file" "rke2-haproxy_userdata" {
+  count    = local.nodes.haproxy
+  template = file("cloud-init/user-data-haproxy.yml")
+  vars = {
+    HOSTNAME           = "rke2-haproxy${format("%02d", count.index + 1)}"
+    SSH_AUTHORIZED_KEY = local.ssh_authorized_key
+  }
+}
+
+resource "esxi_guest" "rke2-haproxy" {
+  count              = local.nodes.haproxy
+  guest_name         = "rke2-haproxy${format("%02d", count.index + 1)}"
+  power              = "on"
+  disk_store         = var.DISK_STORE
+  clone_from_vm      = "template-ubuntu2004"
+  memsize            = local.memsize
+  numvcpus           = local.numvcpus
+  resource_pool_name = esxi_resource_pool.kubernetes.resource_pool_name
+  boot_disk_size     = "15"
+  network_interfaces {
+    virtual_network = esxi_portgroup.portgroup100_1.name
+    mac_address     = "00:50:56:00:64:${format("%02X", 5 + count.index)}"
+  }
+  network_interfaces {
+    virtual_network = esxi_portgroup.portgroup101_1.name
+    mac_address     = "00:50:56:00:65:${format("%02X", 5 + count.index)}"
+  }
+  guestinfo = {
+    "metadata.encoding" = "gzip+base64"
+    "metadata"          = base64gzip(data.template_file.rke2_metadata.rendered)
+    "userdata.encoding" = "gzip+base64"
+    "userdata"          = base64gzip(data.template_file.rke2-haproxy_userdata[count.index].rendered)
+  }
 }
 
 
@@ -65,11 +104,11 @@ resource "esxi_guest" "rke2-server" {
   boot_disk_size     = "15"
   network_interfaces {
     virtual_network = esxi_portgroup.portgroup100_1.name
-    mac_address     = "00:50:56:00:64:${format("%02X", count.index + 4)}"
+    mac_address     = "00:50:56:00:64:${format("%02X", 5 + local.nodes.haproxy + count.index)}"
   }
   network_interfaces {
     virtual_network = esxi_portgroup.portgroup101_1.name
-    mac_address     = "00:50:56:00:65:${format("%02X", count.index + 4)}"
+    mac_address     = "00:50:56:00:65:${format("%02X", 5 + local.nodes.haproxy + count.index)}"
   }
   virtual_disks {
     virtual_disk_id = esxi_virtual_disk.rke2-server_1[count.index].id
@@ -116,11 +155,11 @@ resource "esxi_guest" "rke2-agent" {
   boot_disk_size     = "15"
   network_interfaces {
     virtual_network = esxi_portgroup.portgroup100_1.name
-    mac_address     = "00:50:56:00:64:${format("%02X", local.nodes.server + count.index + 4)}"
+    mac_address     = "00:50:56:00:64:${format("%02X", 5 + local.nodes.haproxy + local.nodes.server + count.index)}"
   }
   network_interfaces {
     virtual_network = esxi_portgroup.portgroup101_1.name
-    mac_address     = "00:50:56:00:65:${format("%02X", local.nodes.server + count.index + 4)}"
+    mac_address     = "00:50:56:00:65:${format("%02X", 5 + local.nodes.haproxy + local.nodes.server + count.index)}"
   }
   virtual_disks {
     virtual_disk_id = esxi_virtual_disk.rke2-agent_1[count.index].id
