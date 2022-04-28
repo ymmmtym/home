a63b6ca1abf28c64c0d7a75f89bb2ed491838d10
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2022-04-28 20:52:22 +0900

    Launch `default` cluster

--- terraform/guest-rke2.tf
+++ terraform/guest-rke2.tf
@@ -8,7 +8,7 @@ locals {
   ssh_authorized_key = file("~/.ssh/kube.id_rsa.pub")
 
   nodes = {
-    haproxy = 2
+    haproxy = 0
     server  = 3
     agent   = 3
   }
@@ -33,8 +33,28 @@ data "template_file" "rke2_metadata" {
   template = file("cloud-init/meta-data-ubuntu.yml")
 }
 
+resource "random_password" "rke2_token" {
+  length           = 16
+  special          = true
+  override_special = "!#$%&*()-_=+[]{}<>:?"
+}
+
 
 ### HAProxy for RKE2 Server
+
+data "template_file" "rke2-haproxy_haproxy" {
+  template = file("haproxy/haproxy.cfg.tftpl")
+}
+
+data "template_file" "rke2-haproxy_keepalived" {
+  count    = local.nodes.haproxy
+  template = file("keepalived/keepalived.conf.tftpl")
+  vars = {
+    STATE     = count.index == 0 ? "MASTER" : "BACKUP"
+    INTERFACE = "ens32"
+    PRIORITY  = count.index == 0 ? "150" : "100"
+  }
+}
 
 data "template_file" "rke2-haproxy_userdata" {
   count    = local.nodes.haproxy
@@ -42,6 +62,8 @@ data "template_file" "rke2-haproxy_userdata" {
   vars = {
     HOSTNAME           = "rke2-haproxy${format("%02d", count.index + 1)}"
     SSH_AUTHORIZED_KEY = local.ssh_authorized_key
+    HAPROXY_CFG        = base64gzip(data.template_file.rke2-haproxy_haproxy.rendered)
+    KEEPALIVED_CONF    = base64gzip(data.template_file.rke2-haproxy_keepalived[count.index].rendered)
   }
 }
 
@@ -74,12 +96,36 @@ resource "esxi_guest" "rke2-haproxy" {
 
 ### RKE2 Server
 
+data "template_file" "rke2-server_keepalived" {
+  count    = local.nodes.server
+  template = file("keepalived/keepalived.conf.tftpl")
+  vars = {
+    STATE     = count.index == 0 ? "MASTER" : "BACKUP"
+    INTERFACE = "ens32"
+    PRIORITY  = count.index == 0 ? "150" : "100"
+  }
+}
+
+data "template_file" "rke2-server_config" {
+  count    = local.nodes.server
+  template = file("rke2/config.yaml.tftpl")
+  vars = {
+    FIRST_SERVER       = count.index == 0 ? "" : "rke2-server01"
+    TOKEN              = random_password.rke2_token.result
+    INSTALL_RKE2_TYPE  = "server"
+    TLS_SAN            = "[rke2-server, rke2-server01, rke2-server02, rke-server03]"
+  }
+}
+
 data "template_file" "rke2-server_userdata" {
   count    = local.nodes.server
   template = file("cloud-init/user-data-rke2.yml")
   vars = {
+    KEEPALIVED_CONF    = base64gzip(data.template_file.rke2-server_keepalived[count.index].rendered)
     HOSTNAME           = "rke2-server${format("%02d", count.index + 1)}"
     SSH_AUTHORIZED_KEY = local.ssh_authorized_key
+    INSTALL_RKE2_TYPE  = "server"
+    RKE2_CONFIG        = base64gzip(data.template_file.rke2-server_config[count.index].rendered)
   }
 }
 
@@ -122,8 +168,39 @@ resource "esxi_guest" "rke2-server" {
   }
 }
 
+resource "null_resource" "update_kubeconfig" {
+  triggers = {
+    rke2-server-ids = join(",", esxi_guest.rke2-server.*.id)
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOF
+    TMP_KUBECONFIG=$(mktemp)
+    rm $TMP_KUBECONFIG
+    while [ ! -e $TMP_KUBECONFIG ]; do scp rke2-server:~/.kube/config $TMP_KUBECONFIG; sleep 10; done
+    sed -i.bak "s/127\.0\.0\.1/rke2-server/g" $TMP_KUBECONFIG
+    kubectl config delete-context default
+    kubectl config delete-cluster default
+    kubectl config delete-user default
+    NEW_KUBECONFIG=$(KUBECONFIG=~/.kube/config:$TMP_KUBECONFIG kubectl config view --flatten)
+    IFS=''
+    echo $NEW_KUBECONFIG > ~/.kube/config
+    rm $TMP_KUBECONFIG{,.bak}
+    EOF
+  }
+}
+
 
 ### RKE2 Agent
+
+data "template_file" "rke2-agent_config" {
+  template = file("rke2/config.yaml.tftpl")
+  vars = {
+    FIRST_SERVER       = "rke2-server01"
+    TOKEN              = random_password.rke2_token.result
+    INSTALL_RKE2_TYPE  = "agent"
+  }
+}
 
 data "template_file" "rke2-agent_userdata" {
   count    = local.nodes.agent
@@ -131,6 +208,8 @@ data "template_file" "rke2-agent_userdata" {
   vars = {
     HOSTNAME           = "rke2-agent${format("%02d", count.index + 1)}"
     SSH_AUTHORIZED_KEY = local.ssh_authorized_key
+    INSTALL_RKE2_TYPE  = "agent"
+    RKE2_CONFIG        = base64gzip(data.template_file.rke2-agent_config.rendered)
   }
 }
 
