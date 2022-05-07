5e27b5bcd69b196f12503c0c140b0bf6fbc0591a
ymmmtym <26668352+ymmmtym@users.noreply.github.com>
2022-05-08 08:13:29 +0900

    upgrade argo-cd helm version

--- terraform/guest-rke2.tf
+++ terraform/guest-rke2.tf
@@ -363,7 +363,7 @@ resource "helm_release" "argo-cd" {
   name       = "argo-cd"
   repository = "https://argoproj.github.io/argo-helm"
   chart      = "argo-cd"
-  version    = "4.5.7"
+  version    = "4.5.11"
   namespace  = "argocd"
 
   create_namespace = true
